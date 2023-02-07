2658f668ba96df03d204f0275d2a15779f0bb694
adrien-glg <75178159+adrien-glg@users.noreply.github.com>
2023-02-07 23:05:51 +0100

    sfn_constants.py with new config fashion (ini)

--- step_functions/sfn_constants.py
+++ step_functions/sfn_constants.py
@@ -1,11 +1,17 @@
 import sfn_config
 
-import importlib
-from lambda_code.src import generic_constants
-constants = importlib.import_module(generic_constants.CONSTANTS_MODULE, package=None)
+from configparser import ConfigParser
 
+config_parser = ConfigParser()
+config_parser.read('../lambda_code/projectname_config.ini')
 
-FUNCTION_NAME = constants.PROJECT_NAME + "_" + str(constants.NUMBER_OF_SLICES) + "_slices"
+PROJECT_NAME = config_parser.get('project', 'project_name')
+
+config_parser.read('../lambda_code/projects/' + PROJECT_NAME + "/" + PROJECT_NAME + "_config.ini")
+
+NUMBER_OF_SLICES = config_parser.getint('number_of_slices', 'number_of_slices')
+
+FUNCTION_NAME = PROJECT_NAME + "_" + str(NUMBER_OF_SLICES) + "_slices"
 STATE_MACHINE_NAME = FUNCTION_NAME + "_" + sfn_config.STATE_MACHINE_SUFFIX
 EVENT0 = "../events/event0.json"
 STATE_MACHINE_ARN_FILE="outputs/state_machine_arn.txt"
